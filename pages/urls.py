1751c420a9647dc4b3008c26a6c539d4a06f4116
bhargav9529 <vbnbhargav95@gmail.com>
2021-03-30 15:50:29 +0530

    created templates

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('services', views.services, name='services'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'page/home.html')
+def index(request):
+    return render(request, 'page/index.html')
+
+
+def about(request):
+    return render(request, 'page/about.html')
+
+
+def contact(request):
+    return render(request, 'page/contact.html')
+
+
+def services(request):
+    return render(request, 'page/services.html')
